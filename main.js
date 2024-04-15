74ac2ba6b3f8b052a70a45d9ec1391b7a499baec
musanurbadr <111696681+musanurbadr@users.noreply.github.com>
2024-04-15 10:38:40 +0300

    Ping-single-column-coming-soon-page

---
+++ main.js
@@ -0,0 +1,20 @@
+function showInput() {
+  const email = document.getElementById('email-address-input').value;
+  const errorMessage = document.getElementById('errorMessage');
+  const successMessage = document.getElementById('successMessage');
+
+  // Öğelerin varlığını kontrol et
+  if (errorMessage && successMessage) {
+    if (validateEmail(email)) {
+      successMessage.textContent = `E-posta adresiniz: ${email}`;
+      errorMessage.textContent = '';
+    } else {
+      errorMessage.textContent = 'The email address is not formatted correctly';
+      successMessage.textContent = '';
+    }
+  }
+}
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
