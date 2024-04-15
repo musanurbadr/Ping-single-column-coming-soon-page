45cc45c2f2b2340918bbec43df3b9014dae3740a
musanurbadr <111696681+musanurbadr@users.noreply.github.com>
2024-04-15 16:48:11 +0300

    Ping-single-column-coming-soon-page

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 function showInput() {
-  const email = document.getElementById('email-address-input').value;
-  const errorMessage = document.getElementById('errorMessage');
-  const successMessage = document.getElementById('successMessage');
+const email = document.getElementById('email-address-input').value;
+const errorMessage = document.getElementById('errorMessage');
+const successMessage = document.getElementById('successMessage');
 
   // Öğelerin varlığını kontrol et
-  if (errorMessage && successMessage) {
+if (errorMessage && successMessage) {
     if (validateEmail(email)) {
-      successMessage.textContent = `E-posta adresiniz: ${email}`;
-      errorMessage.textContent = '';
+    successMessage.textContent = `E-posta adresiniz: ${email}`;
+    errorMessage.textContent = '';
     } else {
-      errorMessage.textContent = 'The email address is not formatted correctly';
-      successMessage.textContent = '';
+    errorMessage.textContent = 'The email address is not formatted correctly';
+    successMessage.textContent = '';
     }
-  }
+}
 }
 function validateEmail(email) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
 }
